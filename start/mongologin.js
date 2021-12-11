a2ab25a6ba81eb44f382909e56457229b800f03d
GabrielDGamer <gabrielrleite2011@gmail.com>
2021-12-11 03:18:03 -0300

    master

--- start/mongologin.js
+++ start/mongologin.js
@@ -1,8 +1,5 @@
 const { MongoClient } = require('mongodb');
 
-var dbname = "DatabaseCDV";
-var dbpass = "jarvis5661330";
-
 const urlDB = `mongodb+srv://gabrieldgamer:${dbpass}@databasecdv.rfh9y.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 const clientDB = new MongoClient(urlDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
